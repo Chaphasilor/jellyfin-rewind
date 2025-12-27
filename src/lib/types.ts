b889c3a0acfd85a33cb090e32e356ce71bf8a19a
Chaphasilor <ppp.u@web.de>
2025-12-27 18:52:55 +0100

    add legacy types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -114,3 +114,409 @@ export type Listen = {
     ClientName: string;
     PlaybackMethod: string;
 };
+
+// old Rewind report format
+
+export type FullRewindReport = {
+    jellyfinRewindReport: {
+        commit: string;
+        year: number;
+        timestamp: string;
+        user: {
+            id: string;
+            name: string;
+        },
+        server: {
+            LocalAddress: string;
+            ServerName: string;
+            Version: string;
+            ProductName: string;
+            OperatingSystem: string;
+            Id: string;
+            StartupWizardCompleted: boolean;
+            PublicAddress: string;
+        },
+        type: "full";
+        playbackReportAvailable: boolean;
+        playbackReportDataMissing: boolean;
+        generalStats: {
+            totalPlaybackDurationByMonth: {
+                0: number;
+                1: number;
+                2: number;
+                3: number;
+                4: number;
+                5: number;
+                6: number;
+                7: number;
+                8: number;
+                9: number;
+                10: number;
+                11: number;
+            },
+            totalPlays: {
+                playbackReport: number;
+                average: number;
+                jellyfin: number;
+            },
+            totalPlaybackDurationMinutes: {
+                playbackReport: number;
+                average: number;
+                jellyfin: number;
+            },
+            totalPlaybackDurationHours: {
+                playbackReport: number;
+                average: number;
+                jellyfin: number;
+            },
+            uniqueTracksPlayed: number;
+            uniqueAlbumsPlayed: number;
+            uniqueArtistsPlayed: number;
+            playbackMethods: {
+                playCount: {
+                    [method: string]: number;
+                },
+                duration: {
+                    [method: string]: number;
+                },
+            },
+            locations: {
+                devices: {
+                    [device: string]: number;
+                },
+                clients: {
+                    [client: string]: number;
+                },
+                combinations: {
+                    [combination: string]: {
+                        device: string;
+                        client: string;
+                        playCount: number;
+                    };
+                }
+            },
+            mostSuccessivePlays: {
+                track: OldTrack,
+                name: string;
+                artists: OldBaseInfo[];
+                albumArtist: OldAlbumArtistBaseInfo;
+                image: OldImage;
+                playCount: number;
+                totalDuration: number;
+            },
+            totalMusicDays: number;
+            minutesPerDay: {
+                mean: number;
+                median: number;
+                // min, max?
+            }
+        },
+        tracks: {
+            duration: OldTrack[];
+            playCount: OldTrack[];
+            leastSkipped: OldTrack[];
+            mostSkipped: OldTrack[];
+            forgottenFavoriteTracks: OldTrack[];
+        },
+        albums: {
+            duration: OldAlbum[];
+            playCount: OldAlbum[];
+        },
+        artists: {
+            duration: OldArtist[];
+            playCount: OldArtist[];
+        },
+        genres: {
+            duration: OldGenre[];
+            playCount: OldGenre[];
+        },
+        libraryStats: {
+            tracks: {
+                total: number;
+                favorite: number;
+            },
+            albums: {
+                total: number;
+            },
+            artists: {
+                total: number;
+            },
+            trackLength: {
+                mean: number;
+                median: number;
+                min: number;
+                max: number;
+            },
+            totalRuntime: number;
+        },
+        playbackReportComplete: false,
+    },
+    rawData: any;
+}
+export type LightRewindReport = {
+    commit: string;
+    year: number;
+    timestamp: string;
+    user: {
+        id: string;
+        name: string;
+    },
+    server: {
+        LocalAddress: string;
+        ServerName: string;
+        Version: string;
+        ProductName: string;
+        OperatingSystem: string;
+        Id: string;
+        StartupWizardCompleted: boolean;
+        PublicAddress: string;
+    },
+    type: "light";
+    playbackReportAvailable: boolean;
+    playbackReportDataMissing: boolean;
+    generalStats: {
+        totalPlaybackDurationByMonth: {
+            0: number;
+            1: number;
+            2: number;
+            3: number;
+            4: number;
+            5: number;
+            6: number;
+            7: number;
+            8: number;
+            9: number;
+            10: number;
+            11: number;
+        },
+        totalPlays: {
+            playbackReport: number;
+            average: number;
+            jellyfin: number;
+        },
+        totalPlaybackDurationMinutes: {
+            playbackReport: number;
+            average: number;
+            jellyfin: number;
+        },
+        totalPlaybackDurationHours: {
+            playbackReport: number;
+            average: number;
+            jellyfin: number;
+        },
+        uniqueTracksPlayed: number;
+        uniqueAlbumsPlayed: number;
+        uniqueArtistsPlayed: number;
+        playbackMethods: {
+            playCount: {
+                [method: string]: number;
+            },
+            duration: {
+                [method: string]: number;
+            },
+        },
+        locations: {
+            devices: {
+                [device: string]: number;
+            },
+            clients: {
+                [client: string]: number;
+            },
+            combinations: {
+                [combination: string]: {
+                    device: string;
+                    client: string;
+                    playCount: number;
+                };
+            }
+        },
+        mostSuccessivePlays: {
+            track: OldTrack,
+            name: string;
+            artists: OldBaseInfo[];
+            albumArtist: OldAlbumArtistBaseInfo;
+            image: OldImage;
+            playCount: number;
+            totalDuration: number;
+        },
+        totalMusicDays: number;
+        minutesPerDay: {
+            mean: number;
+            median: number;
+            // min, max?
+        }
+    },
+    tracks: {
+        duration: OldTrack[];
+        playCount: OldTrack[];
+        leastSkipped: OldTrack[];
+        mostSkipped: OldTrack[];
+        forgottenFavoriteTracks: OldTrack[];
+    },
+    albums: {
+        duration: OldAlbum[];
+        playCount: OldAlbum[];
+    },
+    artists: {
+        duration: OldArtist[];
+        playCount: OldArtist[];
+    },
+    genres: {
+        duration: OldGenre[];
+        playCount: OldGenre[];
+    },
+    libraryStats: {
+        tracks: {
+            total: number;
+            favorite: number;
+        },
+        albums: {
+            total: number;
+        },
+        artists: {
+            total: number;
+        },
+        trackLength: {
+            mean: number;
+            median: number;
+            min: number;
+            max: number;
+        },
+        totalRuntime: number;
+    },
+    playbackReportComplete: false,
+}
+
+interface OldBaseInfo {
+  id: string;
+  name: string;
+}
+
+interface OldAlbumArtistBaseInfo {
+  id: string;
+  name: string;
+}
+
+interface OldAlbumBaseInfo {
+  id: string;
+  name: string;
+  albumArtistBaseInfo: OldAlbumArtistBaseInfo;
+}
+
+interface OldImage {
+  parentItemId: string;
+  primaryTag: string;
+  blurhash: string;
+}
+
+interface OldSkipScore {
+  jellyfin: number;
+  playbackReport: number;
+  average: number;
+}
+
+interface OldSkips {
+  partial: number;
+  full: number;
+  total: number;
+  score: OldSkipScore;
+}
+
+interface OldPlayCount {
+  jellyfin: number;
+  playbackReport: number;
+  average: number;
+}
+
+interface OldPlay {
+  date: string;
+  duration: number;
+  wasFullSkip: boolean;
+  wasPartialSkip: boolean;
+  client: string;
+  device: string;
+  method: string;
+}
+
+interface OldMostSuccessivePlays {
+  playCount: number;
+  totalDuration: number;
+}
+
+interface OldTotalPlayDuration {
+  jellyfin: number;
+  playbackReport: number;
+  average: number;
+}
+
+interface OldTrack {
+  name: string;
+  id: string;
+  artistsBaseInfo: OldBaseInfo[];
+  albumBaseInfo: OldAlbumBaseInfo;
+  genreBaseInfo: OldBaseInfo[];
+  image: OldImage;
+  year: number;
+  duration: number;
+  skips: OldSkips;
+  playCount: OldPlayCount;
+  plays: OldPlay[];
+  mostSuccessivePlays: OldMostSuccessivePlays;
+  lastPlayed: string;
+  totalPlayDuration: OldTotalPlayDuration;
+  isFavorite: boolean;
+  lastPlay?: string;
+}
+
+interface OldAlbum {
+  name: string;
+  id: string;
+  artists: OldBaseInfo[]; // not deduped
+  albumArtist: OldAlbumArtistBaseInfo;
+  tracks: OldTrack[];
+  year: number | string; // ISO string
+  image: OldImage;
+  playCount: OldPlayCount;
+  plays: OldPlay[];
+  lastPlayed: string;
+  totalPlayDuration: OldTotalPlayDuration;
+}
+
+interface OldArtist {
+  name: string;
+  id: string;
+  tracks: OldTrack[];
+  images: {
+    primary: OldImage;
+    backdrop: {
+        id: number;
+        parentItemId: string;
+    };
+  };
+  playCount: OldPlayCount;
+  uniqueTracks: OldBaseInfo[];
+  uniquePlayedTracks: {
+    jellyfin: OldBaseInfo[];
+    playbackReport: OldBaseInfo[];
+    average: OldBaseInfo[];
+  };
+  plays: OldPlay[];
+  lastPlayed: string;
+  totalPlayDuration: OldTotalPlayDuration;
+}
+
+interface OldGenre {
+  name: string;
+  id: string;
+  tracks: OldTrack[];
+  playCount: OldPlayCount;
+  uniqueTracks: OldBaseInfo[];
+  uniquePlayedTracks: {
+    jellyfin: OldBaseInfo[];
+    playbackReport: OldBaseInfo[];
+    average: OldBaseInfo[];
+  };
+  plays: OldPlay[];
+  lastPlayed: string;
+  totalPlayDuration: OldTotalPlayDuration;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -61,27 +61,27 @@ Your least partially skipped artists is: {
 Your most listened artists is: {
     $processingResult.artistCache
     .sorted("listenDuration")
-    .at(-1)[1].data
+    .at(-1)?.[1].data
 }<br />
 Your most played artists is: {
     $processingResult.artistCache
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[1].data
+    .at(-1)?.[1].data
 }<br />
 Your most skipped artists is: {
     $processingResult.artistCache
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[1].data
+    .at(-1)?.[1].data
 }<br />
 Your most fully skipped artists is: {
     $processingResult.artistCache
     .sorted("fullSkips")
-    .at(-1)[1].data
+    .at(-1)?.[1].data
 }<br />
 Your most partially skipped artists is: {
     $processingResult.artistCache
     .sorted("partialSkips")
-    .at(-1)[1].data
+    .at(-1)?.[1].data
 }<br /><br /><br /><br />
 
 Your least listened genre is: {
@@ -115,27 +115,27 @@ Your least partially skipped genre is: {
 Your most listened genre is: {
     $processingResult.genresCache
     .sorted("listenDuration")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most played genre is: {
     $processingResult.genresCache
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most skipped genre is: {
     $processingResult.genresCache
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most fully skipped genre is: {
     $processingResult.genresCache
     .sorted("fullSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most partially skipped genre is: {
     $processingResult.genresCache
     .sorted("partialSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br /><br /><br /><br />
 
 Your least listened track is: {
@@ -169,27 +169,27 @@ Your least partially skipped track is: {
 Your most listened track is: {
     $processingResult.tracksCache
     .sorted("listenDuration")
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br />
 Your most played track is: {
     $processingResult.tracksCache
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br />
 Your most skipped track is: {
     $processingResult.tracksCache
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br />
 Your most fully skipped track is: {
     $processingResult.tracksCache
     .sorted("fullSkips")
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br />
 Your most partially skipped track is: {
     $processingResult.tracksCache
     .sorted("partialSkips")
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br /><br /><br /><br />
 
 Your least listened album is: {
@@ -223,27 +223,27 @@ Your least partially skipped album is: {
 Your most listened album is: {
     $processingResult.albumsCache
     .sorted("listenDuration")
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br />
 Your most played album is: {
     $processingResult.albumsCache
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br />
 Your most skipped album is: {
     $processingResult.albumsCache
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br />
 Your most fully skipped album is: {
     $processingResult.albumsCache
     .sorted("fullSkips")
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br />
 Your most partially skipped album is: {
     $processingResult.albumsCache
     .sorted("partialSkips")
-    .at(-1)[1].data.name
+    .at(-1)?.[1].data.name
 }<br /><br /><br /><br />
 
 Your least listened weekday is: {
@@ -277,27 +277,27 @@ Your least partially skipped weekday is: {
 Your most listened weekday is: {
     $processingResult.dayOfWeek
     .sorted("listenDuration")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most played weekday is: {
     $processingResult.dayOfWeek
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most skipped weekday is: {
     $processingResult.dayOfWeek
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most fully skipped weekday is: {
     $processingResult.dayOfWeek
     .sorted("fullSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most partially skipped weekday is: {
     $processingResult.dayOfWeek
     .sorted("partialSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br /><br /><br /><br />
 
 Your least listened month is: {
@@ -331,27 +331,27 @@ Your least partially skipped month is: {
 Your most listened month is: {
     $processingResult.monthOfYear
     .sorted("listenDuration")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most played month is: {
     $processingResult.monthOfYear
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most skipped month is: {
     $processingResult.monthOfYear
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most fully skipped month is: {
     $processingResult.monthOfYear
     .sorted("fullSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most partially skipped month is: {
     $processingResult.monthOfYear
     .sorted("partialSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br /><br /><br /><br />
 
 Your least listened month day is: {
@@ -385,27 +385,27 @@ Your least partially skipped month day is: {
 Your most listened month day is: {
     $processingResult.dayOfMonth
     .sorted("listenDuration")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most played month day is: {
     $processingResult.dayOfMonth
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most skipped month day is: {
     $processingResult.dayOfMonth
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most fully skipped month day is: {
     $processingResult.dayOfMonth
     .sorted("fullSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most partially skipped month day is: {
     $processingResult.dayOfMonth
     .sorted("partialSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br /><br /><br /><br />
 
 Your least listened year day is: {
@@ -439,27 +439,27 @@ Your least partially skipped year day is: {
 Your most listened year day is: {
     $processingResult.dayOfYear
     .sorted("listenDuration")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most played year day is: {
     $processingResult.dayOfYear
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most skipped year day is: {
     $processingResult.dayOfYear
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most fully skipped year day is: {
     $processingResult.dayOfYear
     .sorted("fullSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most partially skipped year day is: {
     $processingResult.dayOfYear
     .sorted("partialSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br /><br /><br /><br />
 
 Your least listened hour is: {
@@ -493,27 +493,27 @@ Your least partially skipped hour is: {
 Your most listened hour is: {
     $processingResult.hourOfDay
     .sorted("listenDuration")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most played hour is: {
     $processingResult.hourOfDay
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most skipped hour is: {
     $processingResult.hourOfDay
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most fully skipped hour is: {
     $processingResult.hourOfDay
     .sorted("fullSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most partially skipped hour is: {
     $processingResult.hourOfDay
     .sorted("partialSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br /><br /><br /><br />
 
 Your least listened device is: {
@@ -547,27 +547,27 @@ Your least partially skipped device is: {
 Your most listened device is: {
     $processingResult.deviceCache
     .sorted("listenDuration")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most played device is: {
     $processingResult.deviceCache
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most skipped device is: {
     $processingResult.deviceCache
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most fully skipped device is: {
     $processingResult.deviceCache
     .sorted("fullSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most partially skipped device is: {
     $processingResult.deviceCache
     .sorted("partialSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br /><br /><br /><br />
 
 Your least listened client is: {
@@ -601,27 +601,27 @@ Your least partially skipped client is: {
 Your most listened client is: {
     $processingResult.clientCache
     .sorted("listenDuration")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most played client is: {
     $processingResult.clientCache
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most skipped client is: {
     $processingResult.clientCache
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most fully skipped client is: {
     $processingResult.clientCache
     .sorted("fullSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most partially skipped client is: {
     $processingResult.clientCache
     .sorted("partialSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br /><br /><br /><br />
 
 Your least listened method is: {
@@ -655,25 +655,25 @@ Your least partially skipped method is: {
 Your most listened method is: {
     $processingResult.playbackCache
     .sorted("listenDuration")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most played method is: {
     $processingResult.playbackCache
     .sorted(["fullPlays", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most skipped method is: {
     $processingResult.playbackCache
     .sorted(["fullSkips", "partialSkips"])
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most fully skipped method is: {
     $processingResult.playbackCache
     .sorted("fullSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br />
 Your most partially skipped method is: {
     $processingResult.playbackCache
     .sorted("partialSkips")
-    .at(-1)[0]
+    .at(-1)?.[0]
 }<br /><br /><br /><br />
